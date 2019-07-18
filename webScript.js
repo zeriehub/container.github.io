aeb7220f391314d19f939a8bfd4c45b9fee984ff
Merhawi <merhawizerie@gmail.com>
2019-07-18 15:31:07 -0500

    initial update

--- webScript.js
+++ webScript.js
@@ -1,1 +1,14 @@
 let count=0;
+let circle=document.createElement("div");
+let btn= document.querySelector("button");
+btn.addEventListener(click,startGame);
+
+function startGame()
+{
+circle.toggle("circle");
+document.body.appendChild(circle);
+circle.style.top="100";
+circle.style.left="300";
+
+
+}
