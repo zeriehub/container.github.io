7340328adb857730426de28e09040c43c0ad44f7
Merhawi <merhawizerie@gmail.com>
2020-08-20 02:06:52 -0500

    Replaced the index, css, and js file with what is on netwbean's web folder

---
+++ Js1.js
@@ -0,0 +1,11 @@
+/* 
+ * Name: Merehaw Zerie
+ * Student ID: 3044369
+ */
+"use strict";
+
+//let booleanValue=true;
+//alert(typeof booleanValue);
+//booleanValue=String(booleanValue);
+//alert(typeof(booleanValue));
+
