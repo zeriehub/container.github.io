e549b5dec64b1199dfa4c79e2c6360c8be8d77e6
Merhawi <merhawizerie@gmail.com>
2020-08-22 03:30:23 -0500

    Background change with some div content on the left of the page

--- Js1.js
+++ Js1.js
@@ -9,3 +9,18 @@
 //booleanValue=String(booleanValue);
 //alert(typeof(booleanValue));
 
+
+let number=+prompt("Enter a Number.");
+let counter=0;
+for(let i=2;i<number;i++)
+{
+    counter=0;
+    for(let j=1;j<=i;j++)
+    {
+        if(i%j===0)
+            {counter++;}
+    }   
+    
+    if(counter===2)
+        console.log(i);
+}
